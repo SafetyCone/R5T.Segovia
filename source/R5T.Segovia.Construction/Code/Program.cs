66a5e2555f07b75dfdfc4b2b6bc129bc0fb6eb85
David Coats <david@getsafetycone.com>
2021-03-19 22:06:29 -0700

    Initial commit of source stub

---
+++ source/R5T.Segovia.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Segovia.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
