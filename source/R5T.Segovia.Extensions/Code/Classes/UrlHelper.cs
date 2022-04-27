15fbfcba2288b043e2ed71852e96174c836da607
davidcoats <david@getsafetycone.com>
2022-04-27 15:34:52 -0700

    Interim changes.

--- source/R5T.Segovia.Extensions/Code/Classes/UrlHelper.cs
+++ source/R5T.Segovia.Extensions/Code/Classes/UrlHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
+using R5T.Magyar;
+
 
 // Use Flurl namespace since adding a reference to this project is indication enough of a desire to use this class.
 namespace Flurl
 {
     public static class UrlHelper
     {
-        public const string RelativeUrlBase = Strings.Empty;
+        public static readonly string RelativeUrlBase = Strings.Empty;
 
 
         public static Url NewRelativeUrl()
